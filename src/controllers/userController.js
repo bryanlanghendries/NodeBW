7ed5406e998d9fcb581406a0c40e770bca79c7a8
Bryan Langhendries <bryan.langhendries@student.ehb.be>
2024-01-11 14:47:27 +0100

    Can now search user by first name

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -96,3 +96,21 @@ exports.deleteUser = async (req, res) => {
         res.status(500).json({ error: 'Internal Server Error' });
     }
 };
+
+exports.searchUsersByFirstName = async (req, res) => {
+    try {
+        const { firstName } = req.query;
+
+        if (!firstName) {
+            return res.status(400).json({ error: 'First name is required for searching.' });
+        }
+
+        const users = await User.find({ firstName });
+
+        res.status(200).json(users);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
